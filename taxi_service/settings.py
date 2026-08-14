44bdca55e0d7a9a11b19879c607f5dbb67e60420
Tymur <t.balenko12008@gmail.com>
2026-08-14 20:00:27 +0200

    fixed test problem

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.middleware.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
